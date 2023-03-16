de3065cb82e0509064bac566c3a9f9965109649c
Bishop-Code-dev <danielnzau2568@gmail.com>
2023-03-16 16:26:06 -0700

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <unistd.h>
 
+/**
+ * main - write to stdout
+ * Return: Always 0
+ */
 int main(void)
 {
-    const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = sizeof(msg) - 1;
-    ssize_t ret = write(STDERR_FILENO, msg, len);
-    return (ret == len) ? 1 : -1;
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
