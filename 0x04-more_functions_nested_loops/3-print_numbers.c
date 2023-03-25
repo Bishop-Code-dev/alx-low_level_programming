51bd603466a555d4218d603a1df937dd9916e6aa
Bishop-Code-dev <danielnzau2568@gmail.com>
2023-03-24 23:59:44 -0700

    3-print_numbers.c correction

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,6 +3,10 @@
 /**
  * print_numbers - print 0-9
  * return: void
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success, the number of characters written is returned.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
 void print_numbers(void)
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,15 @@
+/**
+ * _putchar - writes the character c to stdout
+ *
+ * @c: The character to print
+ *
+ * Return: On success, the number of characters written is returned.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
